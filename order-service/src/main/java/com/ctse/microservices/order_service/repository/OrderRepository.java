37debfe85a111eae447093a63c7e9b7368e87385
Janani Jayasuriya Withana <99182048+Janani-Withana@users.noreply.github.com>
2025-05-11 23:29:18 +0530

    implemented the OrderRepository

--- order-service/src/main/java/com/ctse/microservices/order_service/repository/OrderRepository.java
+++ order-service/src/main/java/com/ctse/microservices/order_service/repository/OrderRepository.java
@@ -1,8 +1,7 @@
 package com.ctse.microservices.order_service.repository;
 
-import com.ctse.microservices.order_service.model.Order;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.ctse.microservices.order_service.model.Order;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
+public interface OrderRepository extends JpaRepository<Order, Long> { }
